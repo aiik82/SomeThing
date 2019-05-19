2a0fd8b627086d7e858c4260a94610521ae674cf
aiik82 <vasartdmi@gmail.com>
2019-05-20 00:36:20 +0300

    ///

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace WpfAppAllCode
+{
+    class Program:Application
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            
+            Program app=new Program();
+            app.Startup+=AppStartUp;
+            app.Exit+=AppExit;
+            app.Run();
+
+
+        }
+
+
+        static void AppExit(object sender,ExitEventArgs e)
+        {
+
+            MessageBox.Show("App Exit");
+
+        }
+        static void AppStartUp(object sender, StartupEventArgs e)
+        {
+            //Window mainWindow=new Window();
+            //mainWindow.Title="My First WPF App in the World!";
+            //mainWindow.Height=200;
+            //mainWindow.Width=300;
+            //mainWindow.WindowStartupLocation=WindowStartupLocation.CenterScreen;
+            //mainWindow.Show();
+            
+            MainWindow wnd=new MainWindow("My better WPF App",500,700);  
+        
+            wnd.Show();
+
+        }
+        class MainWindow:Window
+        {
+            private Button btnExitApp = new Button();
+            
+
+            public MainWindow(string WindowTitle, int height, int width)
+            {
+                btnExitApp.Click += new RoutedEventHandler(btnExitApp_Clicked);
+                btnExitApp.Content = "Exit from Application";
+                btnExitApp.Width = 200;
+                btnExitApp.Height = 75;
+                this.Title=WindowTitle;
+                this.WindowStartupLocation=WindowStartupLocation.CenterScreen;
+                this.Height=height;
+                this.Width=width;               
+                this.AddChild(btnExitApp);
+                this.Show();
+            }
+            private void btnExitApp_Clicked(object sender, RoutedEventArgs e)
+            {
+               this.Close();
+            }
+           
+
+
+        }
+    }
+
+}
